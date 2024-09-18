e65dad2150516f8f277db3c749b73330b4daec8d
sherinrahma <salsaaaulia@gmail.com>
2024-09-18 09:29:28 +0700

    first commit

---
+++ perpus/app/Models/PeminjamanModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PeminjamanModel extends Model
+{
+    protected $table            = 'peminjaman';
+    protected $primaryKey       = 'PeminjamanID';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['UserID', 'BukuID', 'TanggalPeminjaman', 'TanggalPengembalian', 'StatusPeminjaman'];
+ 
+    
+    
+}
+ 
